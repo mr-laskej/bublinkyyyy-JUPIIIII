c896b7198abdc0e1191414ff5bb2e575d09669dc
Matyáš Laský <matyas.lasky.020@pslib.cz>
2022-03-21 14:11:26 +0100

    Přidejte soubory projektu.

---
+++ bublinkyyyy JUPIIIII/bublinkyyyy JUPIIIII.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%d, ", arr[i]);
+	}
+	printf("\n");
+}
+
+void prohodit(int *a, int *b) {
+	int temp = *a;
+	*a = *b; 
+	*b = temp;
+
+}
+
+void bubbleSort(int arr[], int size) {
+	for (int j = 0; j < size - 1; j++) {
+	for (int i = 0; i < size - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			prohodit(&arr[i], &arr[i + 1]);
+		}
+		}
+	}
+}
+
+int main()
+{
+	int pole[] = { 64, 32, 20, 10, 33, 69, 5 };
+	int pocet = sizeof(pole) / sizeof(pole[0]);
+	printArray(pole, pocet);
+	bubbleSort(pole, pocet);
+	printArray(pole, pocet);
+}
